8fac4ac517fef0e96b78a851828742a9ea8e6e4d
cbwj <computerboywj@aliyun.com>
2018-12-12 17:25:53 +0800

    添加异常页与日志记录

---
+++ DbMonitor.WebUI/Infrastructure/Concrete/AppHandleErrorAttribute.cs
@@ -0,0 +1,28 @@
+﻿using DbMonitor.WebUI.Controllers;
+using DbMonitor.WebUI.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DbMonitor.WebUI.Infrastructure.Concrete
+{
+    public class AppHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            int code = HttpContext.Current.Response.StatusCode;
+            Exception error = filterContext.Exception;
+            var message = error.Message;
+            //设置为true，则说明过滤器处理了该异常
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "Exception",
+                ViewData = new ViewDataDictionary<string>(code.ToString())
+            };
+            LogHelper.WriteErrorFormFilter(filterContext.Exception);
+        }
+    }
+}
---
+++ DbMonitor.WebUI/Utility/LogHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+using System.Text;
+
+namespace DbMonitor.WebUI.Utility
+{
+    public class LogHelper
+    {
+        //Logger是直接和应用程序交互的组件,只是产生日志
+        static ILog logger = LogManager.GetLogger(typeof(LogHelper));
+
+        public static void WriteErrorFormFilter(Exception ex)
+        {
+            try
+            {
+                StringBuilder sbInfo = new StringBuilder();
+                //sbInfo.Append("\r\n-----------------start----------------------\r\n");
+                sbInfo.AppendFormat("请求地址：{0}\r\n", HttpContext.Current.Request.Url.AbsoluteUri);
+                sbInfo.AppendFormat("请求类型：{0}\r\n", HttpContext.Current.Request.RequestType);
+                sbInfo.AppendFormat("异常信息：{0}\r\n", ex);
+                //sbInfo.Append("--------------------end---------------------\r\n");
+                logger.Error(sbInfo);
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
