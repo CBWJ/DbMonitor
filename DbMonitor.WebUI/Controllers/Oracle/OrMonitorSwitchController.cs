3b8086b8c42632b65969932b00bdd54ffcf3d4ac
CBWJ <CBWJ@DESKTOP-N3E8BH9>
2019-01-06 12:27:22 +0800

    oracle加入数据库变更SQL

--- DbMonitor.WebUI/Controllers/Oracle/OrMonitorSwitchController.cs
+++ DbMonitor.WebUI/Controllers/Oracle/OrMonitorSwitchController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DbMonitor.DBAccess.Concrete;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -33,6 +35,35 @@ namespace DbMonitor.WebUI.Controllers.Oracle
         [HttpPost]
         public ActionResult Edit(Domain.MonitorManagement manage, FormCollection collection)
         {
+            if(manage.MMOpen == 1)
+            {
+                JsonResult ret = new JsonResult();
+                try
+                {
+                    //加载数据
+                    var dmExample = db.Dictionary.Where(d => d.DTypeCode == "DmExample" && d.DEnable == 1).ToList();
+                    var sql = "";
+                    using (var dal = new OracleDAL(GetSessionConnStr(manage.SCID.Value)))
+                    {
+                        sql = dmExample.Where(d => d.DCode == "临时表").FirstOrDefault().DName;
+                        dal.ExecuteNonQuery(sql);
+                        sql = dmExample.Where(d => d.DCode == "包说明").FirstOrDefault().DName;
+                        dal.ExecuteNonQuery(sql);
+                        sql = dmExample.Where(d => d.DCode == "包体").FirstOrDefault().DName;
+                        dal.ExecuteNonQuery(sql);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ret.Data = JsonConvert.SerializeObject(new
+                    {
+                        status = 1,
+                        message = ex.Message
+                    });
+                    RecordException(ex);
+                    return ret;
+                }
+            }
             return EditModel(manage);
         }
     }
