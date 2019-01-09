b234d3741cb4dee738a3071cf144c8bfaac1caed
CBWJ <CBWJ@DESKTOP-N3E8BH9>
2019-01-09 20:12:29 +0800

    数据库导出设置

--- DbMonitor.WebUI/Controllers/SystemSettingsController.cs
+++ DbMonitor.WebUI/Controllers/SystemSettingsController.cs
@@ -14,6 +14,10 @@ namespace DbMonitor.WebUI.Controllers
         // GET: SystemSettings
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult Edit()
+        {
             //系统设置
             List<Dictionary> dicSys = db.Dictionary.Where(d => d.DTypeCode == "SysSettings" && d.DEnable == 1).ToList();
             return View(dicSys);
@@ -81,6 +85,90 @@ namespace DbMonitor.WebUI.Controllers
                 {
                     code = 1,
                     msg = ex.Message
+                });
+                RecordException(ex);
+            }
+            return ret;
+        }
+
+        public ActionResult OracleExportSettings()
+        {
+            //Oracle导出设置
+            List<Dictionary> dicSys = db.Dictionary.Where(d => d.DTypeCode == "OracleExport" && d.DEnable == 1).ToList();
+            return View(dicSys);
+        }
+        [HttpPost]
+        public ActionResult OracleExportSettings(string oracle_base, string oracle_home, string oracle_sid, string user, string pwd, string backup_dir)
+        {
+            JsonResult ret = new JsonResult();
+            try
+            {
+                var dic = db.Dictionary.Where(d => d.DTypeCode == "OracleExport" && d.DCode == "oracle_base").FirstOrDefault();
+                dic.DName = oracle_base;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "OracleExport" && d.DCode == "oracle_home").FirstOrDefault();
+                dic.DName = oracle_home;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "OracleExport" && d.DCode == "oracle_sid").FirstOrDefault();
+                dic.DName = oracle_sid;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "OracleExport" && d.DCode == "user").FirstOrDefault();
+                dic.DName = user;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "OracleExport" && d.DCode == "pwd").FirstOrDefault();
+                dic.DName = pwd;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "OracleExport" && d.DCode == "backup_dir").FirstOrDefault();
+                dic.DName = backup_dir;
+
+                db.SaveChanges();
+                ret.Data = JsonConvert.SerializeObject(new
+                {
+                    status = 0,
+                    message = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                ret.Data = JsonConvert.SerializeObject(new
+                {
+                    status = 1,
+                    message = ex.Message
+                });
+                RecordException(ex);
+            }
+            return ret;
+        }
+
+        public ActionResult DmExportSettings()
+        {
+            //Oracle导出设置
+            List<Dictionary> dicSys = db.Dictionary.Where(d => d.DTypeCode == "DmExport" && d.DEnable == 1).ToList();
+            return View(dicSys);
+        }
+        [HttpPost]
+        public ActionResult DmExportSettings(string backup_dir, string dm_home, string user, string pwd)
+        {
+            JsonResult ret = new JsonResult();
+            try
+            {
+                var dic = db.Dictionary.Where(d => d.DTypeCode == "DmExport" && d.DCode == "backup_dir").FirstOrDefault();
+                dic.DName = backup_dir;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "DmExport" && d.DCode == "dm_home").FirstOrDefault();
+                dic.DName = dm_home;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "DmExport" && d.DCode == "user").FirstOrDefault();
+                dic.DName = user;
+                dic = db.Dictionary.Where(d => d.DTypeCode == "DmExport" && d.DCode == "pwd").FirstOrDefault();
+                dic.DName = pwd;
+
+                db.SaveChanges();
+                ret.Data = JsonConvert.SerializeObject(new
+                {
+                    status = 0,
+                    message = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                ret.Data = JsonConvert.SerializeObject(new
+                {
+                    status = 1,
+                    message = ex.Message
                 });
                 RecordException(ex);
             }
